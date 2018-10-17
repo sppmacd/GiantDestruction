138b67828d4a70efbed1d4dfbc6d31fd548c685e
sppmacd <42967349+sppmacd@users.noreply.github.com>
2018-10-17 12:06:59 +0200

    Update World.h

--- World.h
+++ World.h
@@ -27,6 +27,7 @@ public:
         void setFlag(int flag);
         void unsetFlag(int flag);
         FloatRect getRect(int x, int y);
+        Block(short code);
     };
 
     bool isCollided(float x, float y, float sx, float sy);
@@ -36,6 +37,7 @@ public:
     void acceleratePlayer(float x, float y);
     void placeBlock(int x, int y);
     void jump();
+    void loadFromFile();
     void respawnPlayer(float x, float y);
     void setBlock(int x, int y, Block& block);
     Block getBlock(int x, int y);
